08e752ce07266a60714df6d7eb2c1b9c5b45aa78
Mahmoud Khalil <midokhalil1987@gmail.com>
2023-04-27 21:06:06 +0200

    remove image from user card

--- app/userCard.tsx
+++ app/userCard.tsx
@@ -1,5 +1,4 @@
 import { DefaultSession } from "next-auth";
-import Image from "next/image";
 
 export function UserCard({ user }: { user: DefaultSession["user"] }) {
   return (
@@ -8,7 +7,6 @@ export function UserCard({ user }: { user: DefaultSession["user"] }) {
         <h2>Current logged in user:</h2>
         <h5>{user?.name}</h5>
         <h5>{user?.email}</h5>
-        <Image src={user?.image} alt={user?.name} fill />
       </div>
     </>
   );
